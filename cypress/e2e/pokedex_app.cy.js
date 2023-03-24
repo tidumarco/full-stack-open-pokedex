cc8a6e0d4eb58a6ee589a33ee0a364216f951ec2
Marco Tidu <tidumarco@gmail.com>
2023-03-24 21:08:45 +0200

    Test

--- cypress/e2e/pokedex_app.cy.js
+++ cypress/e2e/pokedex_app.cy.js
@@ -3,7 +3,7 @@ describe("Pokedex", function () {
     cy.visit("http://localhost:8080");
     cy.contains("ivysaur");
     cy.contains(
-      "Pokémon and Pokémon character names are trademarks of Nintendo."
+      "Pokémon and Pokémon's character names are trademarks of Nintendo."
     );
   });
 });
