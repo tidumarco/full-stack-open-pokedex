e6f6885c95a759a4e4c423faa39e9183b3497ee8
Marco Tidu <tidumarco@gmail.com>
2023-03-24 12:10:19 +0200

    Added cypress test

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     es6: true,
     "jest/globals": true,
+    "cypress/globals": true,
   },
   settings: {
     react: {
@@ -17,7 +18,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module",
   },
-  plugins: ["react", "jest"],
+  plugins: ["react", "jest", "cypress"],
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
--- cypress/e2e/pokedex_app.cy.js
+++ cypress/e2e/pokedex_app.cy.js
@@ -1,5 +1,3 @@
-import { cy } from "local-cypress";
-
 describe("Pokedex", function () {
   it("front page can be opened", function () {
     cy.visit("http://localhost:5001");
