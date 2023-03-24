9d138a80c0c54ed6992cd8f7f5dd6c2215efa512
Marco Tidu <tidumarco@gmail.com>
2023-03-24 12:02:27 +0200

    Added cypress test

--- test/PokemonPage.jest.spec.jsx
+++ test/PokemonPage.jest.spec.jsx
@@ -139,13 +139,4 @@ describe("<PokemonPage />", () => {
     expect(screen.queryByText("Previous")).toBeNull();
     expect(screen.queryByText("Next")).toBeNull();
   });
-  describe("Pokedex", function () {
-    it("front page can be opened", function () {
-      cy.visit("http://localhost:5001");
-      cy.contains("ivysaur");
-      cy.contains(
-        "Pokémon and Pokémon character names are trademarks of Nintendo."
-      );
-    });
-  });
 });
